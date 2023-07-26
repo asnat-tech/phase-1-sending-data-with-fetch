b5c6314e153468608614af987d2de008d38a1470
asnat <asnatkemuma@gmail.com>
2023-07-26 23:34:06 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(name, email) {
+    const url = 'http://localhost:3000/users';
+    const headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    };
+    const data = {
+      name: name,
+      email: email
+    };
+  
+    return fetch(url, {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data=>(data))
+    .catch(error => {
+        const errorMessage = document.createElement('p');
+        errorMessage.textContent = error.message;
+        document.body.appendChild(errorMessage);
+
+    });
+}
+
